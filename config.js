158991623010b6495368df3b9ccb35106745ed8b
AMAN AZAD <83627208+CamoSpect@users.noreply.github.com>
2026-02-19 00:36:02 -0330

    Update Valentine's configuration details

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Lillie",
+    valentineName: "Harleen",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 🔫💝",
+    pageTitle: "Will You Be My Pookie Valentine? 🔫💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -24,17 +24,17 @@ const CONFIG = {
         first: {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes yes yes yes",                                             // Text for "Yes" button
-            noBtn: "No ew",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            noBtn: "No ew I DONT WANT A RELATIONSHIP",                                               // Text for "No" button
+            secretAnswer: "I don't like you, I love you pookie! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
-            startText: "As much as grayson",                                   // Text before the percentage
+            startText: "Slide it cutie",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
             text: "Will you be my Valentine?", // The big question!
-            yesBtn: "YES MY KING",                                             // Text for "Yes" button
+            yesBtn: "YES MY POOKIE COOKIE",                                             // Text for "Yes" button
             noBtn: "Nah. but try clicking on me"                                                 // Text for "No" button
         }
     },
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "omg Im so lucky. marry me pls. 😈🎉💝💖💝💓",
-        message: "You get a hot makeout session or a cuddle session.",
+        title: "omg Im so lucky. marry me pls 😈🎉💝💖💝💓",
+        message: "You get a hot makeout session and a boyfriend.",
         emojis: "❤️💕"  // These will bounce around
     },
 
